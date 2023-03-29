cbe953b2c1d4f40526f33bdc9cb232f60c694e86
AlonsoU202110867 <u202110867@upc.edu.pe>
2023-03-29 10:14:02 -0500

    Cambios Put y Delete. Category Controller

--- API/Controllers/CategoryController.cs
+++ API/Controllers/CategoryController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,26 +12,16 @@ namespace API.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
+        Category[] categories = new Category[]{
+            new Category { Id = 1, Title = "Categoria 1", Year = 2021, Description = "Description 1"},
+            new Category { Id = 2, Title = "Categoria 2", Year = 2022, Description = "Description 2" },
+            new Category { Id = 3, Title = "Categoria 3", Year = 2023, Description = "Description 3"}};
         // GET: api/Category
         [HttpGet(Name = "GetCategories")]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
-
-        //Alonso
-
-        //asdasdasda
-
-        //kkdrive
-
-        //asadadADASDS
-
-        //ffffffffffffff
-
-        //asdasdas
-
-        ///asdasdasdas
 
         // GET: api/Category/5
         [HttpGet("{id}", Name = "GetCategory")]
@@ -47,14 +38,37 @@ namespace API.Controllers
 
         // PUT: api/Category/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult<Category> Put(int id, [FromBody] Category newCategory)
         {
+            var category = Array.Find(categories, t => t.Id == id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            category = newCategory;
+
+            return category;
         }
 
         // DELETE: api/Category/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult Delete(int id)
         {
+            var category = Array.Find(categories, t => t.Id == id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return new ContentResult
+            {
+                ContentType = "text/plain",
+                StatusCode = 200,
+                Content = "Category Deleted Successfully"
+            };
         }
     }
 }
